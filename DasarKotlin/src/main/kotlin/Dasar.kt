5b0b36f0ff8c9cfcf351a115abaa94326a5d0d74
bagasbest <bagasbest80@gmail.com>
2020-08-16 12:49:47 +0700

    update

---
+++ DasarKotlin/src/main/kotlin/Dasar.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // output + new line
+    println("Hello Bagas Pangestu Ganteng")
+
+    //val vs var
+    val name = "Bagas";
+    println(name)
+    print(if (true) "Always true" else "Always false")
+
+    var name1 = "bagas";
+    name1 = "bagas ganteng";
+    print(name1)
+}
