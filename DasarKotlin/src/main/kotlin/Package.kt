32b8f7be05d37cfa066539ddbf6480d146b4d4bf
bagasbest <bagasbest80@gmail.com>
2020-09-20 10:06:59 +0700

    finish dasar kotlin

---
+++ DasarKotlin/src/main/kotlin/Package.kt
@@ -0,0 +1,7 @@
+import com.tokcompany.setrip.util.*
+
+
+fun main() {
+    sayHello("Bagas")
+    sayGoodBye("Bagas")
+}
---
+++ DasarKotlin/src/main/kotlin/com/tokcompany/setrip/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package com.tokcompany.setrip.util
+
+fun sayHello (name:String){
+    println("Hello $name :-)" )
+}
+
+fun sayGoodBye (name:String) {
+    println("Good Bye $name :-(")
+}
