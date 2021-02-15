9c2cd1793e380eb57ed198325f1ff8dd8f1a2b31
bagasbest <bagasbest80@gmail.com>
2021-02-15 20:52:09 +0700

    dasar kotlin

--- DasarKotlin/src/main/kotlin/D01HelloWorld.kt
+++ DasarKotlin/src/main/kotlin/D01HelloWorld.kt
@@ -1,6 +1,22 @@
 
 fun main () {
 
-    println("Hello Bagas")
+//    val text = "Kotlin"
+//    for (char in text) {
+//        print("$char ")
+//    }
+//
+//    val stat = "Kotlin is\"awesome\""
+//    print (stat)
 
+    val user  = setUser("Bagas", 21)
+    println(user)
+    printUser("Bagas")
+
+}
+
+fun setUser (name: String, age: Int) = "Your name is $name, and your age: $age"
+
+fun printUser (name: String) {
+    println("your name is $name")
 }
