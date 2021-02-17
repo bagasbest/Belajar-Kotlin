262f91040f3decf25a52b7d0c4029812af5a24c8
bagasbest <bagasbest80@gmail.com>
2021-02-18 02:49:15 +0700

    SQLite & Firebase

--- DasarKotlin/src/main/kotlin/D01HelloWorld.kt
+++ DasarKotlin/src/main/kotlin/D01HelloWorld.kt
@@ -6,12 +6,30 @@ fun main () {
 //        print("$char ")
 //    }
 //
-//    val stat = "Kotlin is\"awesome\""
+//    val stat = "Kotlin is\"awesome\""y
 //    print (stat)
 
-    val user  = setUser("Bagas", 21)
-    println(user)
-    printUser("Bagas")
+//    val user  = setUser("Bagas", 21)
+//    println(user)
+//    printUser("Bagas")
+
+//    var counter = 1
+//    while (counter <= 7) {
+//        println("Hello $counter")
+//        counter++
+//    }
+
+//    val numberList = mutableListOf(1,2,3,4,5)
+//    numberList.add(2)
+//    numberList.removeAt(3)
+//    numberList.add(1, 2);
+
+    val setOfInteger = setOf(1,2,4,2,3,5,6,5,3,5,7,45,34,5,6)
+
+    for(i in setOfInteger) {
+        println(i);
+    }
+
 
 }
 
