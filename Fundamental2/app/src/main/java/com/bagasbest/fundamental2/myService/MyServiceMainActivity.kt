cc97ddb8fc29bbb804f1b6afc7917ba7980ac2a3
bagasbest <bagasbest80@gmail.com>
2021-03-24 11:08:35 +0700

    service

--- Fundamental2/app/src/main/java/com/bagasbest/fundamental2/myService/MyServiceMainActivity.kt
+++ Fundamental2/app/src/main/java/com/bagasbest/fundamental2/myService/MyServiceMainActivity.kt
@@ -1,13 +1,30 @@
 package com.bagasbest.fundamental2.myService
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.bagasbest.fundamental2.databinding.ActivityMainBinding
 import com.bagasbest.fundamental2.databinding.ActivityMyServiceMainBinding
 
 class MyServiceMainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyServiceMainBinding
+
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            mServiceBound = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +37,25 @@ class MyServiceMainActivity : AppCompatActivity() {
         }
 
         binding.startJobIntentService.setOnClickListener {
-
+            val mStartJobIntentService = Intent(this, MyIntentService::class.java)
+            mStartJobIntentService.putExtra(MyIntentService.EXTRA_DURATION, 5000L)
+            MyIntentService.enqueueWork(this, mStartJobIntentService)
         }
 
         binding.btnStartBoundService.setOnClickListener {
-
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
         }
 
         binding.btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if(mServiceBound) {
+            unbindService(mServiceConnection)
         }
     }
 }
